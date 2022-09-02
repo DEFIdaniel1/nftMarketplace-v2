6eebab3d8a846e079227050c93a4b02dc448a0c5
danala <danala@danalas-MacBook-Pro.local>
2022-09-01 22:22:03 -0600

    initialize project

---
+++ src/frontend/components/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+    return (
+        <div>
+            <h1>The Market</h1>
+            <h2>The NFT Marketplace</h2>
+        </div>
+    )
+}
+
+export default App
