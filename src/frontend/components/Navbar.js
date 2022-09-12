ebb956fd1179efddd36c7ca91d913e1841791974
DEFIdaniel1 <greatprotectionwallet@gmail.com>
2022-09-12 12:01:08 -0600

    navbar responsive burger menu added

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -1,23 +1,39 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.scss'
 
 function Navbar({ account, connectWallet, disconnectWallet }) {
+    const [openNav, setOpenNav] = useState(false)
+    const openNavHandler = () => {
+        setOpenNav(!openNav)
+    }
+
     return (
-        <div className="navbar">
-            <nav className="nav">
+        <div className={!openNav ? 'navbar navbar__closed' : 'navbar navbar__open'}>
+            <div
+                className={!openNav ? 'navBurger navBurger__closed' : 'navBurger navBurger__open'}
+                onClick={openNavHandler}
+            >
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <nav className={!openNav ? 'nav nav__closed' : 'nav nav__open'}>
                 <NavLink to="/">
-                    <span className="nav__item">Home</span>
+                    <span className="nav__item" onClick={openNavHandler}>
+                        Home
+                    </span>
                 </NavLink>
-                <NavLink to="listings">
+                <NavLink to="listings" onClick={openNavHandler}>
                     <span className="nav__item">Listings</span>
                 </NavLink>
-                <NavLink to="my-listings">
+                <NavLink to="my-listings" onClick={openNavHandler}>
                     <span className="nav__item">My Listings</span>
                 </NavLink>
-                <NavLink to="my-purchases">
+                <NavLink to="my-purchases" onClick={openNavHandler}>
                     <span className="nav__item">Purchases</span>
                 </NavLink>
-                <NavLink to="mint">
+                <NavLink to="mint" onClick={openNavHandler}>
                     <span className="nav__item">Mint NFTs</span>
                 </NavLink>
             </nav>
